a1f5f890a9799af4c42216727dc6e2fe04e40a99
Mahajan Dhiraj <mahajandhiru001@gmail.com>
2023-08-20 23:58:05 +0530

    database connected

--- Config/Database.js
+++ Config/Database.js
@@ -1,3 +1,15 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-mongoose.set("strictQuery", false)
+const CONNECTION = process.env.CONNECT_DB;
+mongoose
+  .connect(CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DataBase Connection Establish");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
+require("./Config/Database");
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server working on:   http://localhost:${PORT}`);
